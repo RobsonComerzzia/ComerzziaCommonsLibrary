aad5ceffd277af84acdcd8907da23e083b814261
bpastorelli <brunopastorelli@hotmail.com>
2025-05-30 22:59:00 -0300

    Ajuste

--- src/main/java/com/seidor/comerzzia/commons/api/controller/TicketsController.java
+++ src/main/java/com/seidor/comerzzia/commons/api/controller/TicketsController.java
@@ -1,5 +1,8 @@
 package com.seidor.comerzzia.commons.api.controller;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,23 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seidor.comerzzia.commons.api.controller.openapi.TicketsControllerOpenApi;
+import com.seidor.comerzzia.commons.api.v1.model.CommonsModel;
 import com.seidor.comerzzia.commons.constants.Constants;
 import com.seidor.comerzzia.commons.domain.service.GenerateXmlService;
 import com.seidor.comerzzia.connector.core.security.CheckSecurity;
 
 @RestController
 @RequestMapping(path = "/v1/interno/tickets", produces = MediaType.APPLICATION_JSON_VALUE)
-public class TicketsController {
+public class TicketsController implements TicketsControllerOpenApi<CommonsModel> {
 	
 	@Autowired
 	private GenerateXmlService service;
 	
 	@CheckSecurity.AllCliendIdPermissioes.CanReadWriteAndIsAuthenticated
 	@PostMapping()
+	@Override
 	@ResponseStatus(HttpStatus.OK)
-	public void generate() {
+	public CommonsModel insert() {
 	
+		
+		CommonsModel response = CommonsModel.builder()
+				.protocol(UUID.randomUUID().toString())
+				.dateTimeRequest(LocalDateTime.now())
+				.build();
+		
 		service.generate(Constants.SIM);
+		
+		return response;
 
 	}
 	
