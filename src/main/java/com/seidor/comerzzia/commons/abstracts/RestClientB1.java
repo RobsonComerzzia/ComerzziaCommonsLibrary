82da23c6f7e8fb95dcaf089ff4eae5f86f5ec6a3
bpastorelli <brunopastorelli@hotmail.com>
2025-06-24 15:40:16 -0300

    Ajustes

--- src/main/java/com/seidor/comerzzia/commons/abstracts/RestClientB1.java
+++ src/main/java/com/seidor/comerzzia/commons/abstracts/RestClientB1.java
@@ -14,40 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @MappedSuperclass
 public abstract class RestClientB1<T, R> {
-	
-	protected void postVoid(T body, String url, String apiKey, String token) throws Exception {
-		
-		RestClient restClient = RestClient.builder()
-				.baseUrl(url)
-				.build();
-		
-		try {
-			restClient.post()
-					.uri(uriBuilder -> uriBuilder
-							.build())
-						.body(body != null ? body : null)
-						.header(Constants.API_KEY, apiKey)
-						.header(Constants.TOKEN, token)
-						.retrieve()
-			        .onStatus(httpStatusCode -> httpStatusCode.is2xxSuccessful(), (req, res) -> {
-			        	 String json = new String(res.getBody().readAllBytes());
-			        	 log.info("{} - Sucesso {} : {}", res.getStatusCode(), json);
-			         })
-					.onStatus(httpStatusCode -> httpStatusCode.is4xxClientError(), (req, res) -> {
-						 String json = new String(res.getBody().readAllBytes());
-						 log.error("{} - Erro {} : {}", res.getStatusCode(), json);
-					})
-					.onStatus(httpStatusCode -> httpStatusCode.is5xxServerError(), (req, res) -> {
-					     String json = new String(res.getBody().readAllBytes());
-					     log.error("{} - ERRO {}: {}", res.getStatusCode(), json);
-					})	
-					.toBodilessEntity();		
-		} catch (Exception e) {
-			log.error("Erro: ", e.getLocalizedMessage());
-		}
-		
-	}
-	
 
 	protected ResponseEntity<R> post(T body, String url, String apiKey, String token) throws Exception {
 		
--- src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
+++ src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
@@ -1,24 +1,7 @@
 package com.seidor.comerzzia.commons.abstracts;
 
-import java.sql.SQLIntegrityConstraintViolationException;
+public abstract class SalesReturnBase extends XmlBase {
+	
 
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public abstract class SalesReturnBase<T, ID> extends XmlBase {
-	
-	protected final JpaRepository<T, ID> repository;
-	
-    public SalesReturnBase(JpaRepository<T, ID> repository) {
-        this.repository = repository;
-    }
-	
-	protected T markAsProcesado(T ticket) throws SQLIntegrityConstraintViolationException, ConstraintViolationException, DataIntegrityViolationException {
-		
-		return repository.save(ticket);
-		
-	}
-	
 
 }
