e0e0d6f6439a09b10c81dc2fb839ff7d7458f2da
bpastorelli <brunopastorelli@hotmail.com>
2025-06-25 09:25:49 -0300

    Ajuste

--- src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
+++ src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
@@ -1,7 +1,8 @@
 package com.seidor.comerzzia.commons.abstracts;
 
 public abstract class SalesReturnBase extends XmlBase {
-	
 
+	protected static final String AWS_PREFIX = "AWS";
+	protected static final String GOOGLE_PREFIX = "GOOGLE";
 
 }
