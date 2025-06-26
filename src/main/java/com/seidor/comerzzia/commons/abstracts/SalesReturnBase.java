c590c1bb4003cf786432d82e506c771b3537f868
bpastorelli <brunopastorelli@hotmail.com>
2025-06-26 17:55:08 -0300

    Ajuste

--- src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
+++ src/main/java/com/seidor/comerzzia/commons/abstracts/SalesReturnBase.java
@@ -1,8 +1,14 @@
 package com.seidor.comerzzia.commons.abstracts;
 
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class SalesReturnBase extends XmlBase {
 
 	protected static final String AWS_PREFIX = "AWS";
 	protected static final String GOOGLE_PREFIX = "GOOGLE";
-
+	protected static final String FISCAL_PREFIX = "FISCAL";
+	protected static final String STANDARD_PREFIX = "STANDARD";
+	protected static final String B1_PREFIX = "B1";
+	
 }
